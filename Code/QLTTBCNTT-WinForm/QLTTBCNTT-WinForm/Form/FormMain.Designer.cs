780bb19fe3c2694401f41676027adbd286ca9db8
Your Name <you@example.com>
2023-12-11 08:53:50 +0700

    #6 - Chuc nang dang nhap tu fomrlogin vao formMain

---
+++ Code/QLTTBCNTT-WinForm/QLTTBCNTT-WinForm/Form/FormMain.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows.Forms;
+
+namespace QLTTBCNTT_WinForm
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AccToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accMgrToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panelDesktopPane = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AccToolStripMenuItem
+            // 
+            this.AccToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.accMgrToolStripMenuItem});
+            this.AccToolStripMenuItem.Name = "AccToolStripMenuItem";
+            this.AccToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.AccToolStripMenuItem.Text = "Tài khoản";
+            // 
+            // accMgrToolStripMenuItem
+            // 
+            this.accMgrToolStripMenuItem.Name = "accMgrToolStripMenuItem";
+            this.accMgrToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.accMgrToolStripMenuItem.Text = "Quản lý tài khoản";
+            this.accMgrToolStripMenuItem.Visible = false;
+            this.accMgrToolStripMenuItem.Click += new System.EventHandler(this.accMgrToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AccToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1128, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panelDesktopPane
+            // 
+            this.panelDesktopPane.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelDesktopPane.Location = new System.Drawing.Point(166, 7);
+            this.panelDesktopPane.Name = "panelDesktopPane";
+            this.panelDesktopPane.Size = new System.Drawing.Size(960, 654);
+            this.panelDesktopPane.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 660);
+            this.Controls.Add(this.panelDesktopPane);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "FormMain";
+            this.Text = "Quản lý Trang thiết bị CNTT";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ToolStripMenuItem AccToolStripMenuItem;
+        private ToolStripMenuItem accMgrToolStripMenuItem;
+        private MenuStrip menuStrip1;
+        private Panel panelDesktopPane;
+    }
+}
+
---
+++ Code/QLTTBCNTT-WinForm/QLTTBCNTT-WinForm/Form/FormMain.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
+using QLTTBCNTT_WinForm.Object;
+using QLTTBCNTT_WinForm.suport;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTTBCNTT_WinForm
+{
+    public partial class FormMain : Form
+    {
+        private Form activeForm;
+        private Button currentButton;
+        QueryTK SQL = new QueryTK();
+        private string KindOfAcc = "1";
+        string user = "";
+        public FormMain()
+        {
+            InitializeComponent();
+            this.ControlBox = false;
+        }
+        public FormMain(string _user)
+        {
+            InitializeComponent();
+            user = _user;
+            string _KindOfAcc;
+            
+            _KindOfAcc = SQL.FindByUser(_user).Rows[0][0].ToString();
+            int i = int.Parse(_KindOfAcc);
+            if (_KindOfAcc.Equals("1"))
+            {
+                accMgrToolStripMenuItem.Visible = true;
+            }
+            if (i == 2)
+            {
+                accMgrToolStripMenuItem.Visible = false;
+            }
+            if (i == 3)
+            {
+                accMgrToolStripMenuItem.Visible = false;
+            }
+
+        }
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        
+
+        #region bổ trợ
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button) btnSender)
+                {
+                    //DisnableButton();
+                    //Color color = SelectThemeColor();
+                    //btnCloseChildForm.Visible = true;
+                    currentButton = (Button)btnSender;
+                    //currentButton.BackColor = Color.Pink;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPane.Controls.Add(childForm);
+            //this.panelDesktopPane.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+        #endregion
+
+        private void accMgrToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
