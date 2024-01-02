dee50f30fad5f5ae44844e1af3379e135301e6e0
Theanh09061993 <Theanh09061993@gmail.com>
2024-01-02 11:09:45 +0700

    #25 - add giao dien Xuat bao cao

---
+++ Code/QLTTBCNTT-WinForm/QLTTBCNTT-WinForm/Form/Report.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Reporting.WinForms;
+using QLTTBCNTT_WinForm.Model.ModelTB;
+using QLTTBCNTT_WinForm.Model.ModelTBDV;
+using QLTTBCNTT_WinForm.Object;
+using QLTTBCNTT_WinForm.suport;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTTBCNTT_WinForm
+{
+    public partial class Report : Form
+    {
+        public Report()
+        {
+            InitializeComponent();
+        }
+
+        private void Report_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnReportTB_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnTB_tonkho_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnTBQN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTBDV_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
