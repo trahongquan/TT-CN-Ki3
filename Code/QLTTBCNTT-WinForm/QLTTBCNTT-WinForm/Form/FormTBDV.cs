e3f2531dd565cf291fc6b3c4ca1bc491e83e59c3
phamquangduan <phamquangduansqtt@gmail.com>
2024-01-02 14:33:29 +0700

    #24 - Chuc nang them

--- Code/QLTTBCNTT-WinForm/QLTTBCNTT-WinForm/Form/FormTBDV.cs
+++ Code/QLTTBCNTT-WinForm/QLTTBCNTT-WinForm/Form/FormTBDV.cs
@@ -32,6 +32,16 @@ namespace QLTTBCNTT_WinForm
         #region Button Funcion
         private void AddTBDV_Click(object sender, EventArgs e)
         {
+            if (CheckIDTB_TBDV())
+            {
+                if (Input())
+                {
+                    QueryTBDV.Insert(GetTBDV());
+                    Reload();
+                    dtgvTBDV.Refresh();
+                }
+            }
+            else { return; }
 
         }
         private void ModifyTBDV_Click(object sender, EventArgs e)
@@ -54,6 +64,70 @@ namespace QLTTBCNTT_WinForm
         private void ccbidDV_TextChanged(object sender, EventArgs e)
         {
         }
+
+        #region Bổ trợ
+
+        private TBDonvi GetTBDV()
+        {
+            int idDV = int.Parse(cbbIDDV.Text);
+            int idTB = int.Parse(cbbIDTB.SelectedValue.ToString());
+            //int idTB = int.Parse(cbbIDTB.Text);
+            TBDonvi TBDV = new TBDonvi(idDV, idTB, DateBorrow.Value.ToString(), DateReturn.Value.ToString());
+            return TBDV;
+        }
+        private bool Input()
+        {
+            if (cbbIDDV.Text == "" || cbbIDTB.Text == "")
+            {
+                MessageBox.Show("Không được để trống trường đơn vị và thiết bị");
+                return false;
+            }
+            return true;
+        }
+        private void Clear()
+        {
+            cbbIDDV.Text = "";
+            cbbIDTB.Text = "";
+            txtSearchTBDV.Text = "";
+        }
+
+        private void Reload()
+        {
+            Clear();
+            try
+            {
+                dtgvTBDV.DataSource = QueryTBDV.getDS_TBDonvi();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Display()
+        {
+            var TBDV = dtgvTBDV.SelectedRows[0];
+            cbbIDDV.Text = TBDV.Cells[1].Value.ToString();
+            cbbIDTB.SelectedValue = TBDV.Cells[2].Value;
+            DateBorrow.Value = Convert.ToDateTime(TBDV.Cells[3].Value.ToString());
+            DateReturn.Value = Convert.ToDateTime(TBDV.Cells[4].Value.ToString());
+        }
+
+        private Boolean CheckIDTB_TBDV()
+        {
+            string ds = QueryTBDV.getTBDV_idTB_check(cbbIDTB.SelectedValue.ToString()) /*+ new QueryTBQN().getTBQN_idTB_check(cbbIDTB.SelectedValue.ToString())*/;
+            if (ds.Equals(""))
+            {
+                MessageBox.Show("Thiết bị hợp lệ, chưa được biên chế hoặc cho mượn");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Thiết bị đã được biên chế hoặc được cho mượn. Xin vui lòng chọn thiết bị khác!");
+                cbbIDTB.Text = "";
+                return false;
+            }
+        }
+        #endregion
 
     }
 }
